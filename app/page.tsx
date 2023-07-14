2d1f761542ae3a220f9d5dd55e63f220448cb498
Ryan Henrique <ryanhardflip@gmail.com>
2023-07-13 21:58:42 -0300

    feat

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Characters from "./components/SectionCharacters";
 import HomeSection from "./components/SectionHome";
 import Locations from "./components/SectionLocations";
 import Episodes from "./components/SectionEpisodes";
-
+import Footer from "./components/FooterApp";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-2 p-24 font-black text-3xl text-zinc-800">
@@ -10,6 +10,7 @@ export default function Home() {
       <Characters />
       <Locations />
       <Episodes />
+      <Footer />
     </main>
   );
 }
